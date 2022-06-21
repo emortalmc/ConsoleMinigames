e9f34d978ad3113397e88522088852b6ec5e0f09
emortal <nathanpenwill88@gmail.com>
2022-06-21 23:56:14 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
 
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-    implementation("com.github.EmortalMC:MinestomPvP:9c666df031")
+    implementation("com.github.EmortalMC:MinestomPvP:1be6321fa0")
 }
 
 tasks {
